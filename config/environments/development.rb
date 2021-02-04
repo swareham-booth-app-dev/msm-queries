3ad3d07d2a8da4cdfd3d0141f8f291a35ef8b780
Jelani Woods <jelani@firstdraft.com>
2021-02-04 13:17:11 -0600

    WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,22 +1,26 @@
 Rails.application.configure do
-    config.hosts.clear
-    path = Rails.root.join("whitelist.yml")
-    default_whitelist_path = Rails.root.join("default_whitelist.yml")
-    whitelisted_ips = []
+  config.hosts.clear
+  path = Rails.root.join("whitelist.yml")
+  default_whitelist_path = Rails.root.join("default_whitelist.yml")
+  whitelisted_ips = []
 
-    if File.exist?(path)
-      whitelisted_ips = YAML.load_file(path)
-    end
+  if File.exist?(path)
+    whitelisted_ips = YAML.load_file(path)
+  end
 
-    if File.exist?(default_whitelist_path)
-      whitelisted_ips = whitelisted_ips.concat(YAML.load_file(default_whitelist_path))
-    end
+  if File.exist?(default_whitelist_path)
+    whitelisted_ips = whitelisted_ips.concat(YAML.load_file(default_whitelist_path))
+  end
 
-    config.web_console.permissions = whitelisted_ips
-    config.web_console.whiny_requests = false
+  config.web_console.permissions = whitelisted_ips
+  config.web_console.whiny_requests = false
 
-    BetterErrors::Middleware.allow_ip! '10.138.0.0/16'
+  BetterErrors::Middleware.allow_ip! '10.138.0.0/16'
 
+  # Automatically connect models to database
+  console do
+    ActiveRecord::Base.connection
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
