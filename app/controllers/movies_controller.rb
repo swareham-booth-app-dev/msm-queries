f424d9f0ef5fb8f60feb651f37e7e17abcbf19e4
swareham-booth <swareham@chicagobooth.edu>
2021-04-28 20:20:58 +0000

    Movies top level working

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+    @list_of_directors = Director.all
+    render({ :template => "movies_templates/index.html.erb" })
+  end
+
+  def movie_details
+    the_id = params.fetch("an_id")
+    @the_movie = Movie.where({ :id => the_id }).at(0)
+    @the_director = Director.where({ :id => @the_movie.director_id })[0]
+    render({ :template => "movie_templates/movie.html.erb" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/eldest", { :controller => "directors", :action => "wisest" })
-  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
-  get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })  
+  get("/", { :controller => "application", :action => "homepage" })
+
 end
