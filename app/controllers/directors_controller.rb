898c57bd648e348d73faf7cf8897218132e57a36
swareham-booth <swareham@chicagobooth.edu>
2021-04-28 19:29:44 +0000

    Directors only working (no filmography)

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,25 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/index.html.erb" })
+  end
+
+  def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest.html.erb" })
+  end
+
+  def youngest
+    @youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "director_templates/youngest.html.erb" })
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
+    render({ :template => "director_templates/show.html.erb" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index" })
 end
